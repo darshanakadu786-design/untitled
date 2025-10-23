6777c6cd9642c9310916e45e5989992c70137c37
D.K <128054590+KaduDarshana@users.noreply.github.com>
2025-10-23 16:37:19 +0530

    Basic try–catch example we learn that how exceptions are caught and program continues.

---
+++ src/main/java/multiCatch.java
@@ -0,0 +1,12 @@
+public class multiCatch{
+    public static void main(String[]args){
+        try {
+            int arr[] = {1, 3, 5};
+            System.out.println(arr[5]);
+        }catch(ArithmeticException e) {
+            System.out.println(e);
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println(e);
+        }
+    }
+}
