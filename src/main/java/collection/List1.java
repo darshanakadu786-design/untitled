b5cbeebd9df180dbb2309b3c091adedd5a1cdb55
D.K <128054590+KaduDarshana@users.noreply.github.com>
2025-11-03 15:43:54 +0530

    ### 1. **ArrayList**
    - Backed by **dynamic array**.
    - **Fast random access** (O(1)).
    - **Slow insertion/deletion** in the middle.
    - **Not synchronized**.

---
+++ src/main/java/collection/List1.java
@@ -0,0 +1,13 @@
+package collection;
+import java.util.*;
+public class List1{
+    //Arraylist example
+    public static void main(String[]args){
+        ArrayList <String> list=new ArrayList<>();
+        list.add("Mango");
+        list.add("Papaya");
+        list.add("Lichi");
+        System.out.println(list);
+        System.out.println(list.get(2));
+    }
+}
