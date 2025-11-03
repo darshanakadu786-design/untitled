9a93009aa52871f20891286225f5ee48c8e88c3a
D.K <128054590+KaduDarshana@users.noreply.github.com>
2025-11-03 15:18:11 +0530

    explanation (easy) Encapsulation Example code you hide marks inside the class (private), so you cannot access it directly.
    you need to use getter to fetch the value.

---
+++ src/main/java/Encaptulate_typ1.java
@@ -0,0 +1,14 @@
+class Encaptulate_typ1 {
+    private int marks;
+
+    public void setMarks(int m){
+        if(m>0 && m<100) {
+            marks = m;
+        }
+    }
+    public int getMarks() {
+
+        return marks;
+    }
+}
+
---
+++ src/main/java/Student.java
@@ -0,0 +1,9 @@
+public class Student{
+
+public static void main(String[]args){
+    Encaptulate_typ1 e = new Encaptulate_typ1();
+    e.setMarks(88);
+    System.out.println(e.getMarks());
+}
+
+}
