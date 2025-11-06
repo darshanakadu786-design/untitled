829597ed6580f58db52a82a7e5e2f9aa8405bdd9
D.K <128054590+KaduDarshana@users.noreply.github.com>
2025-11-06 13:50:07 +0530

    WE ARE REMOVING DUPLICATES ELEMENTS FROM ARRAY AND PRINT UNIQUE VALUES KEPEPING ELEMENTS IN SAME ORDER AS INSERTION . We used LinkedHashSet to remove duplicate elements in O(n) time and keep the same order AS INSERTION . LinkedHashSet : → means it will not allow duplicate values → and it will remember the same order as inserted

--- src/main/java/multiCatch.java
+++ src/main/java/multiCatch.java
@@ -2,7 +2,7 @@ public class multiCatch{
     public static void main(String[]args){
         try {
             int arr[] = {1, 3, 5};
-            System.out.println(arr[5]);
+            System.out.println(arr[1]);
         }catch(ArithmeticException e) {
             System.out.println(e);
         }catch(ArrayIndexOutOfBoundsException e){
